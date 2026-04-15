2a723c3205d853d70b91e834b6bc35780644d9b3
Tristan <119907414+plots-TH@users.noreply.github.com>
2026-04-14 20:02:33 -0400

    Step 2 & 3: SEO, Favicon, and Image Optimization complete

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import netlify from "@astrojs/netlify";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://edwardhickey.com", // Add this line
-  integrations: [tailwind()],
+  site: "https://netlify.app", // Use this for now
+  integrations: [tailwind(), sitemap()],
+  adapter: netlify(),
+  image: {
+    domains: ["upload.wikimedia.org", "://compass.com"],
+  },
 });
